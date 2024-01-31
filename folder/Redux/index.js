ee04db673c7887396cfe5a8ad46d7961a9b9478c
thevgavini <77669880+thevgavini@users.noreply.github.com>
2024-01-31 12:05:56 +0530

    Create index.js

---
+++ folder/Redux/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';  // Correct import statement
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// console.log('from index.js')
+
+// store
+const BUY_CAKE = 'BUY_CAKE';
+
+// action type
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'first redux action'
+  };
+}
+
+// reducers
+// (previousState, action) => newState
+const initialState = {
+  numofcakes: 20
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numofcakes: state.numofcakes - 1
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer); // Correct usage of createStore
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
